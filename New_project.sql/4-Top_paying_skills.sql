9efeb927c9e4a8d692d33ffe149f45ae54b1c61d
Rabin <rabin.me3e@gmail.com>
2026-07-22 22:50:43 +0545

    upload sql files

---
+++ New_project.sql/4-Top_paying_skills.sql
@@ -0,0 +1,19 @@
+SELECT 
+skills,
+Round(Avg(salary_year_avg),00) as avg_salary
+FROM
+job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id=skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id=skills_dim.skill_id
+WHERE
+job_title_short ='Data Analyst'
+AND
+salary_year_avg is NOT NULL
+AND 
+job_work_from_home = TRUE
+GROUP BY
+skills
+ORDER BY
+avg_salary ASC
+LIMIT 25;
+
